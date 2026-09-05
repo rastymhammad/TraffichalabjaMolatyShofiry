c3a00a4c15a3c1753b67aec3eb6cb47e5ed898d6
rastymhammad <rastymhammad@gmail.com>
2026-09-05 20:08:59 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+    // بۆ داهاتوو دەتوانین لێرەدا وا بکەین سایتەکە بێ ئینتەرنێتیش بکرێتەوە
+});
