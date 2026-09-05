4ca8706963c6270e610817490c6636c6c0ebe456
rastymhammad <rastymhammad@gmail.com>
2026-09-05 20:48:55 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+const CACHE_NAME = 'traffic-halabja-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/sweetalert2@11'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    // بۆ داهاتوو دەتوانین لێرەدا وا بکەین سایتەکە بێ ئینتەرنێتیش بکرێتەوە
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch new version
+        return response || fetch(event.request);
+      })
+  );
 });
