b64084fbcfff15cbedc62978bad335b829d0b976
rastymhammad <rastymhammad@gmail.com>
2026-09-06 20:03:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,17 @@
-const CACHE_NAME = 'traffic-halabja-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://cdn.jsdelivr.net/npm/sweetalert2@11'
-];
+const CACHE_NAME = 'halabja-traffic-v1';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch new version
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return new Response('سیستەمەکە ئێستا ئۆفلاینە.');
+        })
+    );
 });
